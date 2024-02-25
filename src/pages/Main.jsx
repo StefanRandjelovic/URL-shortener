f36b3ca9fbc4fbd3b44aad089b3840094adc10e9
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-02-25 14:57:23 +0100

    Add LinkShortener component.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,13 @@
 // COMPONENTS
 import Description from "@components/Description.jsx";
+import LinkShortener from "@components/LinkShortener.jsx";
 
 const Main = () => {
   return (
     <>
       <main>
         <Description />
+        <LinkShortener />
       </main>
     </>
   );
