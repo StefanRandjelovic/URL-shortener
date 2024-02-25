14a4ef9b28109c8e4d356945048175ef34a0d787
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-02-25 21:47:35 +0100

    Import styles and Button component.

--- src/components/ShortenedLinks.jsx
+++ src/components/ShortenedLinks.jsx
@@ -1,13 +1,19 @@
 // HELPER FUNCTIONS
 import { handleCopy } from "@helpers/helpers.js";
 
+// STYLES
+import "@styles/ShortenedLinks.scss";
+
+// COMPONENTS
+import Button from "./Button";
+
 const ShortenedLinks = ({ longURL, shortURL }) => {
   return (
-    <div onClick={handleCopy}>
+    <div className="shortened-links" onClick={handleCopy}>
       <p className="longURL">{longURL}</p>
       <div className="rightSide">
         <p className="shortURL">{shortURL}</p>
-        <button>copy</button>
+        <Button classA={'button square'} text='copy'/>
       </div>
     </div>
   );
