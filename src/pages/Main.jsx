ac9d87d7fede9d3ce8fe54b98862098b21ac906f
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-02-25 20:06:00 +0100

    Add a ShorenedLinks component with its dependencies.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,35 @@
 // COMPONENTS
 import Description from "@components/Description.jsx";
 import LinkShortener from "@components/LinkShortener.jsx";
+import ShortenedLinks from "@components/ShortenedLinks";
+// DEV DEPENDENCIES
+import { useAtomValue } from "jotai";
+
+// GLOBAL STATE
+import { shortenedLinks } from "@jotai/store";
 
 const Main = () => {
+  // GLOBAL STATE
+  const linksArr = useAtomValue(shortenedLinks);
+  console.log(linksArr);
+
+  
+
   return (
     <>
       <main>
         <Description />
         <LinkShortener />
+        
+          {linksArr.length > 0 &&
+            linksArr.map((links) => (
+              <ShortenedLinks
+                key={crypto.randomUUID()}
+                longURL={links.long}
+                shortURL={links.short}
+              />
+            ))}
+        
       </main>
     </>
   );
