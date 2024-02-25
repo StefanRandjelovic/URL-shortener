21c0422f969af29fed3c449ba5861ce866ddc098
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-02-25 21:49:56 +0100

    Add section element and style it.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,25 +2,26 @@
 import Description from "@components/Description.jsx";
 import LinkShortener from "@components/LinkShortener.jsx";
 import ShortenedLinks from "@components/ShortenedLinks";
+
 // DEV DEPENDENCIES
 import { useAtomValue } from "jotai";
 
 // GLOBAL STATE
 import { shortenedLinks } from "@jotai/store";
 
+// HELPER FUNCTIONS
+import { sectionStyles } from "@helpers/helpers";
+
 const Main = () => {
   // GLOBAL STATE
   const linksArr = useAtomValue(shortenedLinks);
-  console.log(linksArr);
-
-  
 
   return (
     <>
       <main>
         <Description />
         <LinkShortener />
-        
+        <section style={sectionStyles}>
           {linksArr.length > 0 &&
             linksArr.map((links) => (
               <ShortenedLinks
@@ -29,7 +30,7 @@ const Main = () => {
                 shortURL={links.short}
               />
             ))}
-        
+        </section>
       </main>
     </>
   );
