6b23f902e5814c432bf185bd567d706332130045
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-02-27 22:15:25 +0100

    Add a shortened link instance deleting function.

--- src/components/ShortenedLinks.jsx
+++ src/components/ShortenedLinks.jsx
@@ -7,13 +7,28 @@ import "@styles/ShortenedLinks.scss";
 // COMPONENTS
 import Button from "./Button";
 
-const ShortenedLinks = ({ longURL, shortURL }) => {
+const ShortenedLinks = ({ longURL, shortURL, linksArr, setLinksArr }) => {
+  // INSTANCE DELETING FUNCTION
+  const handleDelete = (event) => {
+    event.stopPropagation();
+    const x = Object.values(linksArr.map((links) => links.long)).indexOf(
+      event.target.parentElement.querySelector(".longURL").innerHTML
+    );
+    linksArr.splice(x, 1);
+    setLinksArr([...linksArr]);
+  };
+
   return (
     <div className="shortened-links" onClick={handleCopy}>
-      <p className="longURL">{longURL}</p>
+      <div className="leftSide">
+        <p onClick={handleDelete} className="close" title="Delete this entry">
+          +
+        </p>
+        <p className="longURL">{longURL}</p>
+      </div>
       <div className="rightSide">
         <p className="shortURL">{shortURL}</p>
-        <Button classA={'button square'} text='copy'/>
+        <Button classA={"button square"} text="copy" />
       </div>
     </div>
   );
