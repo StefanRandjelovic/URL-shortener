3ef2210dbbf3fe2224f6b33ba7c877dab1c68b26
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-02-28 22:41:00 +0100

    Add code changes for better design emulation.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,16 +1,17 @@
+// STYLES
+import "@styles/Main.scss";
+
 // COMPONENTS
 import Description from "@components/Description.jsx";
 import LinkShortener from "@components/LinkShortener.jsx";
 import ShortenedLinks from "@components/ShortenedLinks";
+import AdvancedStatistics from "@components/AdvancedStatistics";
 
 // DEV DEPENDENCIES
 import { useAtom } from "jotai";
 
 // GLOBAL STATE
 import { shortenedLinks } from "@jotai/store";
-
-// HELPER FUNCTIONS
-import { sectionStyles } from "@helpers/helpers";
 
 const Main = () => {
   // GLOBAL STATE
@@ -20,8 +21,9 @@ const Main = () => {
     <>
       <main>
         <Description />
+        <section className="gray">
         <LinkShortener />
-        <section style={sectionStyles}>
+        <section className='shortened-links-section'>
           {linksArr.length > 0 &&
             linksArr.map((links) => (
               <ShortenedLinks
@@ -32,6 +34,8 @@ const Main = () => {
                 shortURL={links.short}
               />
             ))}
+        </section>
+        <AdvancedStatistics />
         </section>
       </main>
     </>
