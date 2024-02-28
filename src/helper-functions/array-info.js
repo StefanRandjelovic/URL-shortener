3bf6f4bf81dd2efee8c5cf6222588a8b3fb1101e
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-02-28 22:37:59 +0100

    Create array-info helper file with a card rendering information.

---
+++ src/helper-functions/array-info.js
@@ -0,0 +1,19 @@
+const advancedStatisticsArr = [
+  {
+    img: "/src/images/icon-brand-recognition.svg",
+    h1: "Brand Recognition",
+    text: "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.",
+  },
+  {
+    img: "/src/images/icon-detailed-records.svg",
+    h1: "Detailed Records",
+    text: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
+  },
+  {
+    img: "/src/images/icon-fully-customizable.svg",
+    h1: "Fully Customizable",
+    text: "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.",
+  },
+];
+
+export { advancedStatisticsArr };
