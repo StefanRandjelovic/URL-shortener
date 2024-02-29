d7a9c715a282907c90cbaa6f5fbe5b1895dc4486
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-02-29 22:41:02 +0100

    Finish UnderConstruction page.

--- src/pages/UnderConstruction.jsx
+++ src/pages/UnderConstruction.jsx
@@ -1,7 +1,20 @@
+// STYLES
+import "@styles/UnderConstruction.scss";
+
+// SVG
+import Construction from "@images/construction.svg";
+
 const UnderConstruction = () => {
   return (
     <>
-      <main>UnderConstruction</main>
+      <main className="ucm">
+        <img src={Construction} alt="WIP" />
+        <h1>UnderConstruction</h1>
+        <p>
+          Sorry for the inconvenience, but the requested page is currently under
+          construction
+        </p>
+      </main>
     </>
   );
 };
